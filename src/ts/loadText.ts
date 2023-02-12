3648f57ce31538f1c0dbb69023ce8dbca5ba4d8f
Kyrylo Tymchyshyn <kaeser418@gmail.com>
2023-02-12 19:59:39 +0200

    Added ability to upload and read texts

---
+++ src/ts/loadText.ts
@@ -0,0 +1,122 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+export {};
+
+let fileSelect = document.querySelector("#choose-file") as HTMLInputElement;
+const mainContainer = document.querySelector("#main-container") as HTMLDivElement;
+let textsList = document.querySelector(".file-links") as HTMLUListElement;
+
+type Text = {
+    title: string;
+    text: string;
+};
+
+const textsArr: Text[] = JSON.parse(localStorage.getItem("texts")!) || [];
+
+const paragraphize = (text: string) => {
+    return text
+        .split(/(?:\r?\n)+/)
+        .map((paragraph: string) => `<p>${paragraph}</p>`)
+        .join("");
+};
+
+const readText = (title: string, text: string) => {
+    const refresh = `${window.location.protocol}//${window.location.host}${window.location.pathname}?title=${title}`;
+    window.history.pushState({ path: refresh }, "", refresh);
+
+    mainContainer.classList.remove("center");
+    const paragraphs = paragraphize(text);
+    mainContainer.innerHTML = `
+                <h1>${title}</h1>
+                <div class="scroll">
+                    ${paragraphs}
+                </div>
+                <button id="return">Return</button>
+        `;
+    const returnBtn = document.querySelector("#return") as HTMLButtonElement;
+    // eslint-disable-next-line @typescript-eslint/no-use-before-define
+    returnBtn.addEventListener("click", returnToMain);
+};
+
+const fetchTexts = () => {
+    textsArr.forEach((text) => {
+        const li = document.createElement("li");
+        li.innerText = text.title;
+        li.addEventListener("click", () => readText(text.title, text.text));
+        textsList.appendChild(li);
+    });
+};
+
+const loadText = () => {
+    const reader = new FileReader();
+    reader.readAsText(fileSelect.files![0]);
+    reader.onload = () => {
+        const paragraphs = paragraphize(<string>reader.result);
+        mainContainer.classList.remove("center");
+        mainContainer.innerHTML = `
+                    <input type="text" placeholder="Enter Title" class="input-title" aria-label="Enter Title"/>
+                        <div class="scroll">
+                            ${paragraphs}
+                        </div>
+                    <button id="save-btn">Save</button>
+        `;
+        const inputTitle = document.querySelector(".input-title") as HTMLInputElement;
+        const saveBtn = document.querySelector("#save-btn") as HTMLButtonElement;
+
+        const saveText = () => {
+            if (inputTitle.value.trim() === "") {
+                inputTitle.classList.add("error");
+                inputTitle.parentNode?.querySelector(".error-msg")?.classList.remove("hidden");
+                return;
+            }
+            inputTitle.classList.remove("error");
+            inputTitle.parentNode?.querySelector(".error-msg")?.classList.add("hidden");
+            const textObj = {
+                title: inputTitle.value,
+                text: reader.result as string,
+            };
+            textsArr.push(textObj);
+            localStorage.setItem("texts", JSON.stringify(textsArr));
+        };
+
+        saveBtn.addEventListener("click", saveText);
+    };
+    reader.onerror = () => {
+        console.log("ERROR");
+        mainContainer.innerHTML = "Error Reading File";
+    };
+};
+
+const returnToMain = () => {
+    const refresh = `${window.location.protocol}//${window.location.host}${window.location.pathname}`;
+    window.history.pushState({ path: refresh }, "", refresh);
+    mainContainer.classList.add("center");
+    mainContainer.innerHTML = `
+            <h1>Select File To Read</h1>
+            <div class="file-select-wrapper">
+                <input type="file" accept=".txt" id="choose-file" />
+                <label for="choose-file" class="file-select">Select File</label>
+                <span>No File Selected</span>
+            </div>
+            <h2>Or Select Existing</h2>
+            <ul class="file-links"></ul>
+        `;
+    fileSelect = document.querySelector("#choose-file") as HTMLInputElement;
+    textsList = document.querySelector(".file-links") as HTMLUListElement;
+    fileSelect.addEventListener("change", loadText);
+    fetchTexts();
+};
+
+fileSelect.addEventListener("change", loadText);
+
+fetchTexts();
+
+if (window.location.href.slice(window.location.href.indexOf("?") + 1).includes("title=")) {
+    try {
+        const urlParams = new URLSearchParams(window.location.search);
+        const title = urlParams.get("title");
+        const text = textsArr.find((txt) => txt.title === title)?.text;
+        readText(<string>title, <string>text);
+    } catch {
+        returnToMain();
+    }
+}
